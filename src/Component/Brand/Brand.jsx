e7f66c237c83703f3a6bbd321c6bcbdefd248f98
shubham007kumar <you@example.com>
2021-06-11 14:27:29 +0530

    build product page

---
+++ src/Component/Brand/Brand.jsx
@@ -0,0 +1,126 @@
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { data, data1 } from "./data.js";
+import { useDispatch, useSelector } from "react-redux";
+import { get } from "../../Redux/Product/action.js";
+import styles from './Brand.module.css'
+import styled from 'styled-components'
+import {Navbar} from '../Navigation/Navbar'
+import {Slide} from '../ImageSlider/Slide'
+import { VscLoading } from "react-icons/vsc";
+import { BiRupee } from "react-icons/bi";
+import { FaStar,FaStarHalfAlt} from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
+
+const Div=styled.div`
+background-color:#eceff1;
+`
+
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  root:{
+      width: "120%"
+  }
+}));
+
+export const Brand = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const product = useSelector((state) => state.Product.productList);
+  const isLoading = useSelector((state) => state.Product.isLoading);
+  useEffect(() => {
+    dispatch(get());
+  }, [dispatch]);
+  console.log(product);
+  return (
+    <Div>
+        <Navbar/>
+        <Slide/>
+        <p className={styles.downslide}>All Product</p>
+      <div className={styles.maincontainer}>
+        <div>
+          <div className={classes.root}>
+            <div>
+              {data.map((item) => {
+                const { heading, details } = item;
+                return (
+                  <Accordion>
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+                    >
+                      <Typography className={classes.heading}>
+                        {heading}
+                      </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography>
+                        {details.map((item) => {
+                          return <li>{item}</li>;
+                        })}
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                );
+              })}
+            </div>
+            <div>
+              {data1.map((item) => {
+                const { heading, details } = item;
+                return (
+                  <Accordion>
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+                    >
+                      <Typography className={classes.heading}>
+                        {heading}
+                      </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography>
+                        {details.map((item) => {
+                          return <li>{item}</li>;
+                        })}
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className={styles.productcontainer}>
+            <div className={styles.subcontainer}>
+            {
+        isLoading ? <div className={styles.loading}><VscLoading/></div> :   product.map((item)=>{
+                    return(
+                        <div key={item.id} className={styles.container}>
+                            <img className={styles.imagecontainer} src={item.Image} alt="blank"/>
+                            <p className={styles.namecontainer}>{item.ProduceName}</p>
+                            <p className={styles.pricecontainer}>MRP: <BiRupee/>{item.Price} | {item.Discount}% off</p>
+                            <p className={styles.namecontainer}><FaStar/><FaStar/><FaStar/><FaStarHalfAlt/><FaStarHalfAlt/> {item.Rating}</p>
+                            <div className={styles.hide}>
+                                <AiOutlineHeart/>
+                                <div className={styles.addcart}>Add to Cart</div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+      </div>
+      </div>
+    </Div>
+  );
+};
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,12 +1,14 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from 'redux-thunk'
 import { Loginreducer } from "./Login/reducers";
+import { Productreducer } from "./Product/reducer";
 import { Signupreducer } from "./Signup/reducer";
 
 
 const rootReducer=combineReducers({
    Signup:Signupreducer,
-   Login:Loginreducer
+   Login:Loginreducer,
+   Product:Productreducer
 })
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
