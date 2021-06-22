657a2cf349fcad7dfc929f73eeaab50d8e83dcfc
SHUBHAM KUMAR <34106521+shubham007kumar@users.noreply.github.com>
2021-06-22 11:30:57 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyBsaTwsVHAQ7fSS9DdwD5Ab7ooaaPecsaA",
-  authDomain: "nykaa-development.firebaseapp.com",
-  projectId: "nykaa-development",
-  storageBucket: "nykaa-development.appspot.com",
-  messagingSenderId: "581343408470",
-  appId:"dee7b9b8af688424b266f7",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId:"",
 });
 export const auth = app.auth();
 export default app;
